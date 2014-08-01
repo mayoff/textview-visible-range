3c6a3f72140e3e73b1149eba2478eba865bb0ee9
Rob Mayoff <git@rob.dqd.com>
2014-08-01 11:44:03 -0500

    implement test app

--- textViewRange/ViewController.m
+++ textViewRange/ViewController.m
@@ -8,20 +8,38 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (strong, nonatomic) IBOutlet UITextView *sourceTextView;
+@property (strong, nonatomic) IBOutlet UITextView *targetTextView;
 
 @end
 
 @implementation ViewController
-            
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+- (void)viewDidLayoutSubviews {
+    [self updateTargetTextView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    [self updateTargetTextView];
 }
+
+- (void)updateTargetTextView {
+    NSRange range = [self visibleRangeOfTextView:self.sourceTextView];
+    NSString *visibleText = [self.sourceTextView.text substringWithRange:range];
+    NSAttributedString *trib = [[NSAttributedString alloc] initWithString:visibleText attributes:@{
+        NSFontAttributeName: [UIFont systemFontOfSize:9],
+        NSBackgroundColorAttributeName: [UIColor lightGrayColor]}];
+    self.targetTextView.attributedText = trib;
+}
+
+- (NSRange)visibleRangeOfTextView:(UITextView *)textView {
+    CGRect bounds = textView.bounds;
+    UITextPosition *start = [textView characterRangeAtPoint:bounds.origin].start;
+    UITextPosition *end = [textView characterRangeAtPoint:CGPointMake(CGRectGetMaxX(bounds), CGRectGetMaxY(bounds))].end;
+    return NSMakeRange([textView offsetFromPosition:textView.beginningOfDocument toPosition:start],
+        [textView offsetFromPosition:start toPosition:end]);
+}
+
 
 @end
